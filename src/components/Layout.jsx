3b70f164b3e3fce53feaee5db4c57a0a51acf2f1
Jon Hirsch <jonmh99@gmail.com>
2022-05-09 14:25:42 -0700

    add nav menu for small screens

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,34 +1,71 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useState } from "react";
 import "@fontsource/quattrocento-sans";
 import "@fontsource/quicksand";
 import "@fontsource/roboto";
 import "../styles/styles.scss";
 import Helmet from "react-helmet";
+import { CSSTransition } from "react-transition-group";
 
 export default function Layout(props) {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div className="page">
-      <Helmet>
-        <title>JonHirsch.dev</title>
-      </Helmet>
-      <div className="page-header">
-        <Link className="home-link" to="/">
-          jonhirsch.dev
-        </Link>
-        <div className="navigation">
-          <Link to="/">Home</Link>
-          <Link to="/personal-projects">Personal Projects</Link>
-          <Link to="/articles">Articles</Link>
-          <Link to="/search">Search</Link>
+    <>
+      <div className="page">
+        <Helmet>
+          <title>JonHirsch.dev</title>
+        </Helmet>
+        <div className="page-header">
+          <Link className="home-link" to="/">
+            jonhirsch.dev
+          </Link>
+          <div className="navigation">
+            <Link to="/">Home</Link>
+            <Link to="/personal-projects">Personal Projects</Link>
+            <Link to="/articles">Articles</Link>
+            <Link to="/search">Search</Link>
+          </div>
+          <button
+            className="nav-hamburger"
+            role="button"
+            aria-label="open navigation menu"
+            onClick={() => setShowMenu(true)}
+          >
+            <div className="hamburger-line"></div>
+            <div className="hamburger-line"></div>
+            <div className="hamburger-line"></div>
+          </button>
         </div>
-        <button className="nav-hamburger" aria-label="open navigation menu">
-          <div className="hamburger-line"></div>
-          <div className="hamburger-line"></div>
-          <div className="hamburger-line"></div>
-        </button>
+        <div className="page-content">{props.children}</div>
       </div>
-      <div className="page-content">{props.children}</div>
-    </div>
+      <CSSTransition
+        in={showMenu}
+        classNames="menu-transition"
+        timeout={400}
+        mountOnEnter={true}
+        unmountOnExit={true}
+      >
+        <div className="menu-overlay" onClick={() => setShowMenu(false)}>
+          <div className="close-menu-button-container">
+            <button
+              className="close-menu-button"
+              role="button"
+              aria-label="close navigation menu"
+              onClick={() => setShowMenu(false)}
+            >
+              <div className="close-button-line"></div>
+              <div className="close-button-line"></div>
+            </button>
+          </div>
+          <div className="menu-links">
+            <Link to="/">Home</Link>
+            <Link to="/personal-projects">Personal Projects</Link>
+            <Link to="/articles">Articles</Link>
+            <Link to="/search">Search</Link>
+          </div>
+        </div>
+      </CSSTransition>
+    </>
   );
 }
